6f663384cc06f9d44d7965ef1c29db92d8ec6097
尹明 <50118028@qq.com>
2018-05-19 20:20:07 +0800

    asdasd

--- S_O_Briage.h
+++ S_O_Briage.h
@@ -1,13 +1,2 @@
-//
-//  S_O_Briage.h
-//  CTC
-//
-//  Created by yinming on 2018/5/14.
-//  Copyright © 2018年 CTCORZ. All rights reserved.
-//
-
-#ifndef S_O_Briage_h
-#define S_O_Briage_h
-#import "WXApi.h"
-
-#endif /* S_O_Briage_h */
+asjdajsdlasjld
+asjdasjdlk
